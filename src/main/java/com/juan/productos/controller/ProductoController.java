5598e0af1d6cd63c254339fd389383ad2cef5601
juangiraldomunoz23-ui <juangiraldomunoz23@gmail.com>
2026-06-11 20:31:34 -0500

    Fix Dockerfile

--- src/main/java/com/juan/productos/controller/ProductoController.java
+++ src/main/java/com/juan/productos/controller/ProductoController.java
@@ -16,13 +16,43 @@ public class ProductoController {
         this.repository = repository;
     }
 
+    // Obtener todos los productos
     @GetMapping
     public List<Producto> listar() {
         return repository.findAll();
     }
 
+    
+    @GetMapping("/{id}")
+    public Producto obtenerPorId(@PathVariable Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+   
     @PostMapping
     public Producto guardar(@RequestBody Producto producto) {
         return repository.save(producto);
+    }
+
+  
+    @PutMapping("/{id}")
+    public Producto actualizar(@PathVariable Long id,
+                                @RequestBody Producto productoActualizado) {
+
+        Producto producto = repository.findById(id).orElse(null);
+
+        if (producto != null) {
+            producto.setNombre(productoActualizado.getNombre());
+            producto.setPrecio(productoActualizado.getPrecio());
+
+            return repository.save(producto);
+        }
+
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
